3fe16881710042b3991f406d2f76f4c7220a49f1
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-20 20:09:18 +0100

    Create divs Inside Container

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const size = 16;
 
 // Set Container Grid Templates
 container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+
+// Create Square Grid with Nested Loops
+for(let x = 1; x <= size; x++) {
+    for(let y = 1; y<= size; y++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+    }
+}
