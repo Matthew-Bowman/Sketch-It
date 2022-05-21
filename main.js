70eef12dde1ad0bb1cd9fa970c91dfb54241c300
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-21 01:54:00 +0100

    Moved Initialisation Code into Function

--- main.js
+++ main.js
@@ -5,34 +5,38 @@ const size = 16;
 let drawColour = "#000000";
 let backgroundColour = "#ffffff"
 
-// Set Container Grid Templates
-container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
 
-// Create Square Grid with Nested Loops
-for(let x = 1; x <= size; x++) {
-    for(let y = 1; y<= size; y++) {
-        // Create div
-        const div = document.createElement("div");
+// FUNCTIONS
+function Init() {
+    // Set Container Grid Templates
+    container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
 
-        // Give div Class
-        div.classList.add("tile");
+    // Create Square Grid with Nested Loops
+    for (let x = 1; x <= size; x++) {
+        for (let y = 1; y <= size; y++) {
+            // Create div
+            const div = document.createElement("div");
 
-        // Give div Attributes
-        div.setAttribute(`data-xpos`, x);
-        div.setAttribute(`data-ypos`, y);
-        div.setAttribute(`data-touched`, false);
+            // Give div Class
+            div.classList.add("tile");
 
-        // Add Event Listeners for Drawing to div
-        div.addEventListener("mouseover", e => {
-            // If Mouse Button 1 Pressed
-            if(e.buttons == 1) 
-                Draw(e.target);
-        });
+            // Give div Attributes
+            div.setAttribute(`data-xpos`, x);
+            div.setAttribute(`data-ypos`, y);
+            div.setAttribute(`data-touched`, false);
 
-        div.addEventListener("mousedown", e => Draw(e.target));
+            // Add Event Listeners for Drawing to div
+            div.addEventListener("mouseover", e => {
+                // If Mouse Button 1 Pressed
+                if (e.buttons == 1)
+                    Draw(e.target);
+            });
 
-        // Add div to Container
-        container.appendChild(div);
+            div.addEventListener("mousedown", e => Draw(e.target));
+
+            // Add div to Container
+            container.appendChild(div);
+        }
     }
 }
 
@@ -54,7 +58,7 @@ function UpdateBackground(pColour) {
 
     // Iterate Through Tiles
     tiles.forEach(tile => {
-        if(tile.getAttribute(`data-touched`) == `false`)
+        if (tile.getAttribute(`data-touched`) == `false`)
             tile.style.background = colour;
     })
 }
